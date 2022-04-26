ed96c6703ef74290fb2ec51ef7ddea8b3cde02ad
peizhongli <peizhongli@baidu.com>
2022-04-26 20:23:05 +0800

    add comments

--- myPromise.js
+++ myPromise.js
@@ -1,4 +1,6 @@
+// Promises/A+: https://promisesaplus.com/
 class MyPromise {
+    // A promise must be in one of three states: pending, fulfilled, or rejected.     
     static PENDING = 'pending'
     static FULFILLED = 'fulfilled'
     static REJECTED = 'rejected'
@@ -16,7 +18,9 @@ class MyPromise {
     }
 
     _resolve(result) {
+        // 2.2.1 When pending, a promise: may transition to either the fulfilled or rejected state. “must not change” means immutable identity (i.e. ===), but does not imply deep immutability.
         if (this._state === MyPromise.PENDING) {
+            // onFulfilled or onRejected must not be called until the execution context stack contains only platform code.
             setTimeout(() => {
                 this._state = MyPromise.FULFILLED
                 this._result = result
@@ -38,9 +42,21 @@ class MyPromise {
             });
         }
     }
-
+    // A promise’s then method accepts two arguments, Both onFulfilled and onRejected are optional arguments
     then(onFulfilled, onRejected) {
+        /** 
+            * 2.2.2 If onFulfilled is a function:
+            * 2.2.2.1 it must be called after promise is fulfilled, with promise’s value as its first argument.
+            * 2.2.2.2 it must not be called before promise is fulfilled.
+            * 2.2.2.3 it must not be called more than once.
+        */
         onFulfilled = typeof onFulfilled === 'function' ? onFulfilled : result => result
+        /** 
+            * 2.2.3 If onRejected is a function:
+            * 2.2.3.1 it must be called after promise is rejected, with promise’s reason as its first argument.
+            * 2.2.3.2 it must not be called before promise is rejected.
+            * 2.2.3.3 it must not be called more than once.
+        */
         onRejected = typeof onRejected === 'function' ? onRejected : reason => {
             throw reason
         }
@@ -64,18 +80,23 @@ class MyPromise {
                     }
                 })
             } else if (this._state === MyPromise.FULFILLED) {
+                // 2.2.4 onFulfilled or onRejected must not be called until the execution context stack contains only platform code.
                 setTimeout(() => {
                     try {
                         let x = onFulfilled(this._result)
+                        // 2.2.7.1 If either onFulfilled or onRejected returns a value x, run the Promise Resolution Procedure
                         resolvePromise(promise2, x, resolve, reject)
                     } catch (error) {
+                        // 2.2.7.2 If either onFulfilled or onRejected throws an exception e, promise2 must be rejected with e as the reason.
                         reject(error)
                     }
                 })
             } else if (this._state === MyPromise.REJECTED) {
+                // 2.2.4 onFulfilled or onRejected must not be called until the execution context stack contains only platform code.
                 setTimeout(() => {
                     try {
                         let x = onRejected(this._result)
+                        // 2.2.7.1 If either onFulfilled or onRejected returns a value x, run the Promise Resolution Procedure
                         resolvePromise(promise2, x, resolve, reject)
                     } catch (error) {
                         reject(error)
@@ -84,6 +105,7 @@ class MyPromise {
             }
         })
 
+        // 2.2.7 then must return a promise
         return promise2
     }
 
@@ -156,37 +178,49 @@ class MyPromise {
     }
 }
 
+// https://promisesaplus.com/#the-promise-resolution-procedure
 function resolvePromise(promise2, x, resolve, reject) {
+    // 2.3.1 If promise and x refer to the same object, reject promise with a TypeError as the reason.
     if (x === promise2) {
         return reject(new TypeError('Chaining cycle detected for promise'))
     }
 
+    // 2.3.2 If x is a promise, adopt its state
     if (x instanceof MyPromise) {
         if (x._state === MyPromise.PENDING) {
+            // 2.3.2.1 If x is pending, promise must remain pending until x is fulfilled or rejected.
             x.then(
                 y => resolvePromise(promise2, y, resolve, reject),
                 reject
             )
         } else if (x._state === MyPromise.FULFILLED) {
+            // 2.3.2.2 If/when x is fulfilled, fulfill promise with the same value.
             resolve(x._result)
         } else if (x._state === MyPromise.REJECTED) {
+            // 2.3.2.3 If/when x is rejected, reject promise with the same reason.
             reject(x._result)
         }
     } else if (x !== null && (typeof x === 'object' || typeof x === 'function')) {
+        // 2.3.3 Otherwise, if x is an object or function,
         try {
+            // 2.3.3.1 Let then be x.then
             let then = x.then
+            // 2.3.3.4 If then is not a function, fulfill promise with x.
             if (typeof then !== 'function') {
                 resolve(x)
             } else {
+                // 2.3.3.3.3 If both resolvePromise and rejectPromise are called, or multiple calls to the same argument are made, the first call takes precedence, and any further calls are ignored.
                 let called = false;
                 try {
                     then.call(
                         x,
+                        // 2.3.3.3.1 If/when resolvePromise is called with a value y, run [[Resolve]](promise, y).
                         y => {
                             if (called) return
                             called = true
                             resolvePromise(promise2, y, resolve, reject)
                         },
+                        // 2.3.3.3.2 If/when rejectPromise is called with a reason r, reject promise with r.
                         r => {
                             if (called) return
                             called = true
@@ -196,17 +230,21 @@ function resolvePromise(promise2, x, resolve, reject) {
                 } catch (error) {
                     if (called) return
                     called = true
+                    // 2.3.3.3.4 If calling then throws an exception e
                     reject(error)
                 }
             }
         } catch (error) {
+            // 2.3.3.2 If retrieving the property x.then results in a thrown exception e, reject promise with e as the reason.
             reject(error)
         }
     } else {
+        // 2.3.4 If x is not an object or function, fulfill promise with x.
         return resolve(x)
     }
 }
 
+// promises-aplus-tests
 MyPromise.deferred = function () {
     let result = {}
     result.promise = new MyPromise((resolve, reject) => {
